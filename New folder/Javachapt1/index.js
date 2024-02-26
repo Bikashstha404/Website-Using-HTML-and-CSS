5921f3163e026cb124de40c6646b9b082ca73ed7
Bikash Shrestha <stha.bikam99@gmail.com>
2024-02-26 17:18:15 +0545

    Added HTML and CSS for the website

---
+++ New folder/Javachapt1/index.js
@@ -0,0 +1,54 @@
+// 1.Ways to print in Javascript
+// // console.log("Hello world");
+// // alert("Wanna Netflix and Chill?");
+// // document.write("This is document write")
+
+// //2. javascript console Api
+// console.log("Hello world", 4 + 6, "Another log");
+// console.warn("This is a warning")
+// console.error("This is a error")
+
+// 3. javascript variables
+// Container to store data values
+// multi
+// line 
+// comment
+var number1= 42;
+var number2= 69;
+console.log(number1+number2);
+
+// 4.Data types in javascript
+// Number
+var num1=420;
+var num2=69.69
+
+// String
+var str1=" This is a string";
+var str2="This is also a string";
+
+// Object
+var marks={
+    bimal:42,
+    diwas:69,
+    bikash:99.9998,
+}
+console.log(marks);
+
+// Boolens
+var a=true;
+var b=false;
+console.log(a, b)
+
+// var und = undefined;
+var und;
+console.log(und); 
+
+var n = null;
+console.log(n)
+
+/*
+At a very high level, there are two types of data types in javascript
+1. Primitive Data types:(undefined, null, number, string)
+2. Refrence Data types:
+*/
+
